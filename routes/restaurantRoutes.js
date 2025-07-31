49f9793f0cb85acc578ef8631a0c98df69fccd50
Elhama Tokhi <tokhielhama30@gmail.com>
2025-07-31 21:07:11 +0200

    Add GET route to fetch all restaurants from the database

--- routes/restaurantRoutes.js
+++ routes/restaurantRoutes.js
@@ -3,6 +3,9 @@ import pool from "../models/db.js";
 
 const router = Router();
 
+/*-----------------RESTAURANTS-----------------*/
+
+// View all restaurants
 router.get("/restaurants", async (req, res) => {
   try {
     const result = await pool.query("SELECT * FROM restaurants");
@@ -10,6 +13,38 @@ router.get("/restaurants", async (req, res) => {
   } catch (err) {
     console.error(err);
     res.status(500).send("Server Error");
+  }
+});
+
+// Add a new restaurant
+
+router.post("/restaurants", async (req, res) => {
+  const { name, phone, street, city, country, opening_hours, description } =
+    req.body;
+
+  try {
+    const query = `
+      INSERT INTO restaurants (name, phone, street, city, country, opening_hours, description)
+      VALUES ($1, $2, $3, $4, $5, $6, $7)
+      RETURNING *;
+    `;
+
+    const values = [
+      name,
+      phone,
+      street,
+      city,
+      country,
+      opening_hours,
+      description,
+    ];
+
+    const result = await pool.query(query, values);
+
+    res.status(201).json(result.rows[0]);
+  } catch (err) {
+    console.error("Error adding restaurant: ", err);
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
